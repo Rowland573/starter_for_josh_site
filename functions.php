2f144475be7de8589ab297c41448dac40aa0dd72
Rowland573 <rowlandethan573@gmail.com>
2024-03-16 21:38:31 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,3 +11,6 @@ function load_my_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'load_my_scripts');
 ?>
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 

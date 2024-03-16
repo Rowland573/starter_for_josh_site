8894ffafa79dc06c9affdc06be814dd56265c7b0
Rowland573 <rowlandethan573@gmail.com>
2024-03-16 21:38:28 +0800

    Update header.php

--- header.php
+++ header.php
@@ -4,16 +4,15 @@
   <meta charset="<?php bloginfo('charset'); ?>"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
-
-
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,800;1,800&display=swap" rel="stylesheet">
 
 
-
-
+<!--<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="responsive.css" />-->
 <title> <?php
   if (function_exists('is_tag') && is_tag()) {
      single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
@@ -38,43 +37,44 @@
 </title>
 </head>
 <body>
-
-<header class="container-fluid bg1">
-    <div class="container">
-<nav class="navbar navbar-expand-md navbar-dark">
-  <!-- Brand -->
-  <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
-
-  <!-- Toggler/collapsibe Button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">HOME</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">PROJECTS</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">CV</a>
-      </li>
-       <li class="nav-item">
-        <a class="nav-link" href="#">CONTACT</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-<div class="tag">
-<h1>Games Design For The Future</h1>
-</div>
-</div> <!-- container -->
+    <header class="container-fluid bg1">   <!-- div for the main image the stretches to the edge of the page- -->
+        <div class="container">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+      
+        <!-- Toggler/collapsible Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PROJECTS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CV</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT</a>
+              </li>
+          </ul>
+        </div>
+      </nav> 
+   
+        <div class="tag"><!-- div that holds the content in the middle of the page-->
+                  <h1 > Games Design For the Future </h1><!--  the main tag line-->
+  </div>            
+</div><!-- container-->
 
 <!—remove all our css styles and any Javascript code. WordPress automatically calls javascript /jquery elsewhere, calls to javascript are not needed in the header or the footer now- UNLESS you have calls to google or typkit fonts- they can stay-->
 
 <!-- very important that this code is added-->
 <?php wp_head(); ?> 
+
 </header>

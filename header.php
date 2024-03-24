f166370c2c228958f02eb181913e1ec8bcda2d52
Rowland573 <rowlandethan573@gmail.com>
2024-03-24 19:59:44 +0800

    Update header.php

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
 <body>
     <header class="container-fluid bg1">   <!-- div for the main image the stretches to the edge of the page- -->
         <div class="container">
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <nav class="navbar navbar-expand-md navbar-dark">
         <!-- Brand -->
         <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
       

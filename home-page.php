f83eadf08b5d8946eaad3b9624eeb7f0771cadd9
Rowland573 <rowlandethan573@gmail.com>
2024-04-06 21:02:21 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -21,15 +21,18 @@
  
     <section class="container-fluid articlesbg">
             <div class="container">
-                <h3 class="text-center">LATEST ARTICLES</h3>
+                <h3 class="text-center"><?php the_field('titleforarticles'); ?></h3>
                 <div class="row"><!--  a row that gives us access to the BS columns-->
-                  <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src=" http://170.187.231.66/~mesh18/wp-content/themes/starter_for_josh_site/images/port1.jpg">
-                        <h4 class="article-title">A PORTRAIT</h4>
-                        <p class="date" >12 December 2020</p>
-                        <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-                        </p>
-                        <p class="readmore">CONTINUE READING</p>
+                  
+                <div class="col-md-4 text-center">
+                
+<?php $image = get_field('articleimage');?>
+<img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+
+<h4 class="article-title"><?php the_field('articletitle'); ?></h4>
+<p class="date" ><?php the_field('articledate'); ?></p>
+<p class="article-p"><?php the_field('articleexcerpt'); ?> </p>
+<a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
                     </div>
                     <div class="col-md-4 text-center">
                       <img class="img-fluid" alt=“portait” src=" http://170.187.231.66/~mesh18/wp-content/themes/starter_for_josh_site/images/port2.jpg">

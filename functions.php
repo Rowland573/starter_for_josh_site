98598245b77624221d1f69287a6f9758b0686bc1
Rowland573 <rowlandethan573@gmail.com>
2024-04-13 00:07:09 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -30,6 +30,5 @@ register_nav_menus( array(
 add_theme_support('post-thumbnails'); 
 ?>
 
-
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 

3fa9247f90036306b93cf1637b9eb698379838b5
Rowland573 <rowlandethan573@gmail.com>
2024-04-18 08:28:12 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,10 @@
                 <div class="container"><!-- div to hold the content in the middle of the page-->
                     <div class="row"><!-- a row that gives us access to the BS columns-->
                         <div class="col-md-6 ">
-                           <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
+                           <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a> -->
+                           <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+
 
                         </div>
                         <div class="col-md-6 ">
